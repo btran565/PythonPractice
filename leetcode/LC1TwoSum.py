e45cee765ade99e99c2e911dd8b7944c46eea72d
Brandon Tran <btran565@gmail.com>
2025-01-15 19:34:49 -0700

    comments for diff and solution

--- leetcode/LC1TwoSum.py
+++ leetcode/LC1TwoSum.py
@@ -3,8 +3,8 @@ class Solution(object):
         prevMap = {} # val : index
 
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
+            diff = target - n               
+            if diff in prevMap:             #checks to see if compliment value (diff) exists in prevMap hashtable
+                return [prevMap[diff], i]       #returns solution as two elements 
             prevMap[n] = i
         return
