20726b8db162aea9c3f84f8c4fdbba0eae606145
Brandon Tran <btran565@gmail.com>
2025-01-15 19:11:44 -0700

    added lc1, changed wrong variable in one of the lc1768 arguments

--- leetcode/LC1768MergeStringsAlternately.py
+++ leetcode/LC1768MergeStringsAlternately.py
@@ -1,11 +1,11 @@
-class Solution:
-    def mergeStringsAlternately(self, word1: structuredClone, word2: str) -> str:
+class Solution(object):
+    def mergeStringsAlternately(self, word1, word2):
         i,j = 0, 0
 
         res = []
         while i < len(word1) and j < len(word2):
             res.append(word1[i])
-            res.append(word2[2])
+            res.append(word2[j])
             i += 1
             j += 1
         res.append(word1[i:])
