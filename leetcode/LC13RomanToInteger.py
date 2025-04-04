cd34d959674dbe3ffbb018ed8260be3b1b876c52
Brandon Tran <btran565@gmail.com>
2025-04-03 23:15:27 -0700

    pasted solution from leetcode editorial

--- leetcode/LC13RomanToInteger.py
+++ leetcode/LC13RomanToInteger.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def romanToInt(self, s: str) -> int:
 
         roman = {"I" : 1, "V" : 5, "X" : 10, 
@@ -7,4 +7,24 @@ class Solution:
         ans = 0
 
         for i in range(len(s)):
-            if i +1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+            if i +1 < len(s) and roman[s[i]] < roman[s[i+1]]:'''
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        total = values.get(s[-1])
+        for i in reversed(range(len(s) - 1)):
+            if values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]
+            else:
+                total += values[s[i]]
+        return total
