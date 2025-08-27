4653b070a51cc27d948ddab24ae09d9b058032ff
btran565 <btran565@gmail.com>
2025-08-27 16:00:36 -0700

    stack trace error comment

--- bootdotdev/stacktracepractice/main.py
+++ bootdotdev/stacktracepractice/main.py
@@ -1,4 +1,4 @@
-def create_stats_message(strength, wisdom, dexterity):
+def create_stats_message(strength, wisdom, dexterity):  #stack trace aka traceback error
       total = strength + wisdom + dexterity
     msg = f"You have {strength} strength, {wisdom} wisdom, and {dexterity} dexterity for a total of {total} stats.
     return msg
