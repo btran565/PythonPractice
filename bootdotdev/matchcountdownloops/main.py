024cee234e229276d88df672f9d0c63066cc4c39
btran565 <btran565@gmail.com>
2025-09-03 17:28:49 -0700

    prints elipses for each number

--- bootdotdev/matchcountdownloops/main.py
+++ bootdotdev/matchcountdownloops/main.py
@@ -1,7 +1,7 @@
 def countdown_to_start():
     for i in range(10, 0, -1):
         if i !=1:
-            print(i)
+            print(f"{i}...")
         else:
             print(f"{i}...Fight!")
     return i
