7a1b948d21fd8f07502a27c75c350a40e55b653b
btran565 <btran565@gmail.com>
2025-09-03 17:10:33 -0700

    matchcountdown created

---
+++ bootdotdev/matchcountdownloops/main.py
@@ -0,0 +1,19 @@
+def countdown_to_start():
+    # ?
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Counting down to match start:")
+    countdown_to_start()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
+
