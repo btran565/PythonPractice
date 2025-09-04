1a6895e7bf848c3f9d296591f2dc8ce43d0374d1
btran565 <btran565@gmail.com>
2025-09-03 18:47:20 -0700

    added code to xppoints

--- bootdotdev/exppoints/main.py
+++ bootdotdev/exppoints/main.py
@@ -1,3 +1,8 @@
 def calculate_experience_points(level):
-    pass
+    xp_temp = 0
+    xp_total = 0
+    for i in range(1, level+1):
+        xp_temp = (i-1) * 5
+        xp_total += xp_temp
+    return xp_total
 
