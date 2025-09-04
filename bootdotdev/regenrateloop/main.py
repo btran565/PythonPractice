d28de7245ce973a2f5e22c6b3b0e6aa5c301fa59
btran565 <btran565@gmail.com>
2025-09-03 17:06:44 -0700

    created while and return in regenerate

--- bootdotdev/regenrateloop/main.py
+++ bootdotdev/regenrateloop/main.py
@@ -1,2 +1,6 @@
 def regenerate(current_health, max_health, enemy_distance):
-    pass
+    while current_health < max_health and enemy_distance > 3:   #dont quite understand 'and' vs '&' for conditional
+        current_health += 1
+        enemy_distance -= 2
+    
+    return current_health
