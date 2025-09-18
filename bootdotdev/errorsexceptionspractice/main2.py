f40d908c0d6601601f183a4f399f61203fce12fa
Brandon Tran <btran565@gmail.com>
2025-09-18 16:34:39 -0700

    fixed main2

--- bootdotdev/errorsexceptionspractice/main2.py
+++ bootdotdev/errorsexceptionspractice/main2.py
@@ -1,2 +1,7 @@
 def purchase_item(price, gold_available):
+    if gold_available >= price:
+        return gold_available - price
+    
+    else:
+        raise Exception("not enough gold")
     pass
