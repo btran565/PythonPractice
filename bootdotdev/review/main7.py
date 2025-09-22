3181250d994db78ec15f7b216c14032efa3cadba
Brandon Tran <btran565@gmail.com>
2025-09-21 18:39:25 -0700

    main7 complete

--- bootdotdev/review/main7.py
+++ bootdotdev/review/main7.py
@@ -1,2 +1,8 @@
 def join_strings(strings):
-    pass
+    new_string = ""
+    for string in strings:
+        if new_string == "":
+            new_string = string
+        else:
+            new_string += "," + string
+    return new_string
